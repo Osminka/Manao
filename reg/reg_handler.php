36475dfe9642f1de86d1510d16c3ea3506288c79
Osminka <92145646+Osminka@users.noreply.github.com>
2023-02-13 13:53:53 +0300

    Update reg_handler.php

--- reg/reg_handler.php
+++ reg/reg_handler.php
@@ -44,7 +44,7 @@ if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED
         if(isset($_POST["login"])&&isset($_POST["pass"])&&isset($_POST["conf_pass"])&&isset($_POST["email"])&&isset($_POST["name"])) {
             include "../User.php";
             $salt = '$2a$07$R.gJb2U2N.FmZ4hPp1y2CN$';
-            $user = new User($_POST['login'], passwordHash($_POST['password']), $_POST['email'], $_POST['name']);
+            $user = new User($_POST['login'], passwordHash($_POST['pass']), $_POST['email'], $_POST['name']);
 
             Registration($user);
             // echo json_encode(array('success'=> 1, 'message' => "успешная регистрация"), JSON_UNESCAPED_UNICODE);
